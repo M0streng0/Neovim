248f8463852f03c65fa7525644c575c59b061160
Henrique Oliveira <ho.henrique@proton.me>
2024-07-29 17:15:27 +0200

    Formatting fixing + Improved functionality

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,34 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    -- "2rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "file_open_requested",
+					handler = function()
+						require("neo-tree.command").execute({ action = "close" })
+					end,
+				},
+			},
+		})
+	end,
+	keys = {
+		{
+			"<C-n>",
+			function()
+				require("neo-tree.command").execute({
+					toggle = true,
+					source = "filesystem",
+					position = "left",
+					reveal = true,
+				})
+			end,
+		},
+	},
 }
